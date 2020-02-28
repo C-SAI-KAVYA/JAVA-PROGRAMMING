2d9260f42c0ba1bbe07f900c3724fe0242cdc4a3
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-02-28 20:31:32 +0530

    [SaiKavya] adding junittesting

---
+++ JavaProgramming/src/main/java/com/bridgelabz/junittesting/ConvertedTemperatureWithType.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.junittesting;
+
+public class ConvertedTemperatureWithType {
+	double convertedTemperature;
+	char tempType;
+	
+	public ConvertedTemperatureWithType(double ct, char t){
+		convertedTemperature = ct;
+		tempType = t;
+	}
+
+}
