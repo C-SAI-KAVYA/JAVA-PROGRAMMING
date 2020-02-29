fa52728e8382e550bdd0cb5abfee90dff1eec1aa
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-02-29 10:19:18 +0530

    [SaiKavya] adding test cases to test the binary converted and check whether it is power of two or no

--- JavaProgramming/src/main/test/com/bridgelabz/testjunittesting/TestBinary.java
+++ JavaProgramming/src/main/test/com/bridgelabz/testjunittesting/TestBinary.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
 import com.bridgelabz.exceptions.NegativeOrZeroNumberFoundException;
 import com.bridgelabz.junittesting.utils.Utility;
 
