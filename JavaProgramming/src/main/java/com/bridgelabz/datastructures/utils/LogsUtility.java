dbb7335b552bcdb0fa2f5f664d58c9cc1fd48b7b
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-02-29 08:42:37 +0530

    [SaiKavya] adding loggers utility class to setup loggers

--- JavaProgramming/src/main/java/com/bridgelabz/datastructures/utils/LogsUtility.java
+++ JavaProgramming/src/main/java/com/bridgelabz/datastructures/utils/LogsUtility.java
@@ -4,8 +4,7 @@ import org.apache.log4j.Logger;
 
 public class LogsUtility {
 	
-	private LogsUtility() {
-		
+	private LogsUtility() {		
 	}
 	public static final String FILEPATH = "F:\\BridgeLabzFellowship\\JavaProgramming\\logs\\";
 	public static final String CONFIGPATH = "F:\\BridgeLabzFellowship\\JavaProgramming\\loggers\\log4j.properties";
