2fc58aaae817e58a7dc37b95c76b868d4b377d07
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-02-29 10:22:09 +0530

    [SaiKavya] adding test cases to test the monthly payment values

--- JavaProgramming/src/main/test/com/bridgelabz/testjunittesting/TestMonthlyPayment.java
+++ JavaProgramming/src/main/test/com/bridgelabz/testjunittesting/TestMonthlyPayment.java
@@ -18,7 +18,6 @@ public class TestMonthlyPayment {
 	public void testMonthlyPayment_LessThanOrGreaterThanSpecifiedRangeException_12point345And998And58point34() throws LessThanOrGreaterThanSpecifiedRangeException {
 		Utility.monthlyPayment(12.345, 998, 58.34);
 	}
-	
 	@Test
 	public void testMonthlyPayment_0point4523_12And2016And45point23() throws LessThanOrGreaterThanSpecifiedRangeException {
 		assertEquals(0.4523, Utility.monthlyPayment(12, 2016, 45.23),Math.E);
