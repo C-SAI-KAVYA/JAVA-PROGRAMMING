4355b02e740bae406fd603baeb36dacec27ad0bd
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-03-11 11:33:10 +0530

    [SaiKavya] adding class to store the details of the card

---
+++ JavaProgrammingObjectOrientedProgramming/src/main/java/com/bridgelabz/deckofcards/model/CardModel.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.deckofcards.model;
+
+public class CardModel {
+	public String suit;
+	public String rank;
+	
+	public CardModel(String cardSuit, String cardRank) {
+		this.suit = cardSuit;
+		this.rank = cardRank;
+	}
+
+}
