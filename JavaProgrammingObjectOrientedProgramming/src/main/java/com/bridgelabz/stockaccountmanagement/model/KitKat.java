35926149aa0e2222c972e990c81f53ff9ce45bd5
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-03-11 10:45:14 +0530

    [SaiKavya] adding class to store the details of the 2nd stock kit kat

---
+++ JavaProgrammingObjectOrientedProgramming/src/main/java/com/bridgelabz/stockaccountmanagement/model/KitKat.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.stockaccountmanagement.model;
+
+public class KitKat {
+	private String stockName;
+	private int numberOfShares;
+	private double sharePrices;
+	private double totalStockValue;
+
+	public String getStockName() {
+		return stockName;
+	}
+
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+
+	public int getNumberOfShares() {
+		return numberOfShares;
+	}
+
+	public void setNumberOfShares(int numberOfShares) {
+		this.numberOfShares = numberOfShares;
+	}
+
+	public double getSharePrices() {
+		return sharePrices;
+	}
+
+	public void setSharePrices(double sharePrices) {
+		this.sharePrices = sharePrices;
+	}
+
+	public double getTotalStockValue() {
+		return totalStockValue;
+	}
+
+	public void setTotalStockValue(double totalStockValue) {
+		this.totalStockValue = totalStockValue;
+	}
+
+	@Override
+	public String toString() {
+		return "KitKat [stockName=" + stockName + ", numberOfShares=" + numberOfShares + ", sharePrices=" + sharePrices
+				+ ", totalStockValue=" + totalStockValue + "]";
+	}
+	
+	
+
+}
