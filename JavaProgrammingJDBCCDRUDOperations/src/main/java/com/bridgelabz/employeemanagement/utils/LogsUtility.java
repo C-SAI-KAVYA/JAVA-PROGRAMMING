cad35860777ffc6a11afd361b1d6a12082309f79
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-03-16 12:34:30 +0530

    [SaiKavya] required utils to set loggers and take inputs and display outputs

---
+++ JavaProgrammingJDBCCDRUDOperations/src/main/java/com/bridgelabz/employeemanagement/utils/LogsUtility.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeemanagement.utils;
+
+import org.apache.log4j.Logger;
+
+public class LogsUtility {
+	
+	private LogsUtility() {
+		
+	}
+	public static final String FILEPATH = "F:\\BridgeLabzFellowship\\JavaProgrammingJDBCCDRUDOperations\\logs";
+	public static final String CONFIGPATH = "F:\\BridgeLabzFellowship\\JavaProgrammingJDBCCDRUDOperations\\loggers\\log4j.properties";
+	private static Logger logger;
+	
+	public static Logger getLog() {
+		return logger;
+	}
+	public static void setLog(String className) {
+		LogsUtility.logger = Logger.getLogger(className);
+	}
+}
