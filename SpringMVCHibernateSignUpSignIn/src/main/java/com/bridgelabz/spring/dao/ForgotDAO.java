d189861c093ff0faf77c7e642bb1be65ce7b6ccf
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-04-06 22:08:47 +0530

    Delete ForgotDAO.java

--- SpringMVCHibernateSignUpSignIn/src/main/java/com/bridgelabz/spring/dao/ForgotDAO.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.spring.dao;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface ForgotDAO {
-	
-	public boolean resetByEmail(HttpServletRequest request, String email,String pwd);
-	//public RegisterEntity updatePasswordByEmail(String pwd, String mail);
-}
