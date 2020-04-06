e518e439d24e683fb4be2b0e65808de96b0daf34
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-04-06 21:34:56 +0530

    [SaiKavya] adding controller class

---
+++ JDBCCRUDCallableStatements/src/main/java/com/bridgelabz/employee/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.employee.controller;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.bridgelabz.employee.serviceprovider.EmployeeServiceProviderImpl;
+import com.bridgelabz.employee.utils.InputUtility;
+import com.bridgelabz.employee.utils.LogsUtility;
+
+public class EmployeeController {
+	public static void main(String[] args) {
+		EmployeeServiceProviderImpl impl = new EmployeeServiceProviderImpl();
+		LogsUtility.setLog("employeeController");
+		Logger logger = LogsUtility.getLog();
+		System.setProperty("fname", LogsUtility.FILEPATH+"employeeController.log");
+		PropertyConfigurator.configure(LogsUtility.CONFIGPATH);
+		
+		int choice;
+		boolean exit = false;
+		while(!exit) {
+			logger.info("Enter What you would like to do : ");
+			logger.info("\n1. Insert New Employee Details\n2. Read Employee Details Based On ID\n3. Update Employee Details Based On ID\n4. Delete Employee Details Based On ID\n5. exit");
+			choice = InputUtility.intVal();
+			switch(choice) {
+				case 1:
+					impl.insertEmployeeDetails();
+					break;
+				case 2:
+					impl.readEmployeeDetailsOnID();
+					break;
+				case 3:
+					impl.updateEmployeeDetailsOnID();
+					break;
+				case 4:
+					impl.deleteEmployeeDetailsOnID();
+					break;
+				case 5:
+					exit = true;
+					logger.info("Successfully Exited.................................");
+					break;
+				default:
+					logger.info("Invalid Option Selected................................");
+			}
+		}s
+	}
+}
