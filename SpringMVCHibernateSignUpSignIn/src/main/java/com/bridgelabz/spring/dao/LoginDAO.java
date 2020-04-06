da8333fe86d57a85ebd6770d15bb50f14d038d5f
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-04-06 22:09:03 +0530

    Delete LoginDAO.java

--- SpringMVCHibernateSignUpSignIn/src/main/java/com/bridgelabz/spring/dao/LoginDAO.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.spring.dao;
-
-import com.bridgelabz.spring.dto.RegisterDTO;
-
-public interface LoginDAO {
-
-	public RegisterDTO fetchByEmailAndPassword(String mail,String pwd);
-}
