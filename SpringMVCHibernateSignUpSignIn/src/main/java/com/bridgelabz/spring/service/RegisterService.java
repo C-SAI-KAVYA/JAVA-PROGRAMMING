3dcb5581a8f040028bae319cb94e8540430efefd
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-04-08 22:52:19 +0530

    Delete RegisterService.java

--- SpringMVCHibernateSignUpSignIn/src/main/java/com/bridgelabz/spring/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.spring.service;
-
-import com.bridgelabz.spring.dto.RegisterDTO;
-
-public interface RegisterService {
-
-	public boolean validateAndSave(RegisterDTO dto);
-
-}
