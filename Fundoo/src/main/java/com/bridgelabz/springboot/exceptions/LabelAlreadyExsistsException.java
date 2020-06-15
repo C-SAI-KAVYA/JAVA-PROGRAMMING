8605654088095a0d43a64c123202609bb3f37ffa
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-06-15 17:49:28 +0530

    [C SAI KAVYA] adding exception class to indicate that the label is already exsisting

---
+++ Fundoo/src/main/java/com/bridgelabz/springboot/exceptions/LabelAlreadyExsistsException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.sprinboot.exceptions;
+
+public class LabelAlreadyExsistsException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2906883912162738929L;
+	
+	public LabelAlreadyExsistsException(String message) {
+		super(message);
+	}
+
+}
