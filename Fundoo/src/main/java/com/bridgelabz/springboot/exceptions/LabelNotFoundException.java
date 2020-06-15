dac92023559445e8ccbaba5b8f74b2acecf79193
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-06-15 17:54:07 +0530

    [C SAI KAVYA] adding exception class to indicate the apecified label is not found

---
+++ Fundoo/src/main/java/com/bridgelabz/springboot/exceptions/LabelNotFoundException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.sprinboot.exceptions;
+
+public class LabelNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4584013651228462890L;
+	
+	public LabelNotFoundException(String message) {
+		super(message);
+	}
+
+}
