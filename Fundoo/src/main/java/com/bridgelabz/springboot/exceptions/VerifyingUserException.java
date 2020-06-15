fd1e5366251ccf425c2cd234c00adb4966dbe310
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-06-15 17:57:54 +0530

    [C SAI KAVYA] adding Exception to indicate error in verifying the user

---
+++ Fundoo/src/main/java/com/bridgelabz/springboot/exceptions/VerifyingUserException.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.sprinboot.exceptions;
+
+public class VerifyingUserException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8237023282082041211L;
+	
+	public VerifyingUserException(String message) {
+		super(message);
+	}
+}
