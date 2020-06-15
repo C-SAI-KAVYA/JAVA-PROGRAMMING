3738fa1defc7db9aed54540b77ac3bb6fc89ed39
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-06-15 17:46:34 +0530

    [C SAI KAVYA] adding exception class for interruption while colloborating

---
+++ Fundoo/src/main/java/com/bridgelabz/springboot/exceptions/CollaboratorException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.sprinboot.exceptions;
+
+public class CollaboratorException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1660950817440770568L;
+	
+	public CollaboratorException(String message) {
+		super(message);
+	}
+
+}
