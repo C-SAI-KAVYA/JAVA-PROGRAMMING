82b5a3aa1a75723c881166ae5d344c9e1e0172f7
Tulasi Sai Kavya <56569026+C-SAI-KAVYA@users.noreply.github.com>
2020-06-15 17:53:09 +0530

    [C SAI KAVYA] adding exception class to indicate the specified label is not added

---
+++ Fundoo/src/main/java/com/bridgelabz/springboot/exceptions/LabelNotAddedException.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.sprinboot.exceptions;
+
+public class LabelNotAddedException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5601326007709936932L;
+	public LabelNotAddedException(String message) {
+		super(message);
+	}
+}
